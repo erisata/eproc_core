55560fd666c660014423d7846c49eda9f5f1c281
Karolis Petrauskas <k.petrauskas@gmail.com>
2013-11-03 11:30:26 +0200

    FSM structure.

--- src/eproc_fsm.erl
+++ src/eproc_fsm.erl
@@ -28,18 +28,36 @@
 %%    * Has support for scopes. The scopes can be used to manage timers and keys.
 %%    * Supports automatic state persistence.
 %%
+%%  Several states are maintained during lifeycle of the process:
+%%    * `initializing` - while FSM initializes itself asynchronously.
+%%    * `running`   - when the FSM is running.
+%%    * `suspended` - when the FSM is suspended by an administrator.
+%%    * `faulty'    - when the FSM is suspended because of errors.
+%%
+%%  FSM can reach the following terminal states:
+%%    * `{done, success}` - when the FSM was completed successfully.
+%%    * `{done, failure}` - when the FSM was completed by the callback module returning special response TODO.
+%%    * `{term, killed}`  - when the FSM was killed in the `running` or the `suspended` state.
+%%    * `{term, failed}`  - when the FSM was terminated in the `faulty` state.
+%%
 -module(eproc_fsm).
-%-behaviour(gen_server).
+-behaviour(gen_fsm).
 -compile([{parse_transform, lager_transform}]).
--export([start_link/6, call/2, cast/2, kill/2, suspend/2, resume/2, set_state/4]).
+-export([start_link/6, call/2, call/3, cast/2, kill/2, suspend/2, resume/2, set_state/4]).
 -export([reply/2]).
--export_type([inst_id/0, inst_ref/0]).
+-export([init/1, handle_event/3, handle_sync_event/4, handle_info/3, terminate/3, code_change/4]).
+-export([initializing/2, initializing/3, running/2, running/3, suspended/2, suspended/3, faulty/2, faulty/3]).
+-export_type([name/0, inst_id/0, inst_ref/0]).
 -include("eproc.hrl").
 
 
+%%
+%%  Structure, passed to all callbacks, to be used to indicate process.
+%%
 -record(inst_ref, {
 }).
 
+-type name() :: {via, Registry :: module(), InstanceId :: inst_id()}.
 -opaque inst_id()   :: integer().
 -opaque inst_ref()  :: #inst_ref{}.
 -type state_event() :: term().
@@ -48,6 +66,13 @@
 -type state_action() :: term().
 -type state_phase() :: event | entry | exit.
 
+-record(state, {
+    inst_id,
+    module,
+    registry,
+    store,
+    options
+}).
 
 %% =============================================================================
 %%  Callback definitions.
@@ -114,80 +139,198 @@
 %%
 %%  Start new eproc_fsm instance.
 %%
+%%  `Name` is used to identify the FSM instance. Name should always be provided
+%%      in the form of `{via, module(), term()}`. This notation is compatible with
+%%      the standard OTP process registry behaviour. The registry provided in this
+%%      parameter is also used to reference other processes (can be overriden using options).
+%%  `Module` is a callback module implementing `eproc_fsm` behaviour.
+%%  `Args` is passed to the `Module:init/3` function as the `Args` parameter. This
+%%      is similar to the Args parameter in `gen_fsm` and other OTP behaviours.
+%%  `Event` stands for the initial event of the FSM, i.e. event that created the FSM.
+%%      This event is used for invoking state transition callbacks for the transition
+%%      from the `initial` state.
+%%  `Store' is used to persist FSM state. One can use either void, transient or
+%%      persistent store implementations.
+%%
 -spec start_link(
-        Name    :: {via, Registry :: module(), InstanceId :: inst_id()},
+        Name    :: name(),
         Module  :: module(),
         Args    :: term(),
         Event   :: state_event(),
         Store   :: store_ref(),
         Options :: proplist()
         ) ->
-        {ok, pid()} | term().
+        {ok, pid()} | ignore | {error, term()}.
 
-start_link(_Name, _Module, _Args, _Event, _Store, _Options) ->
-    ok.
-
-
-%%
-%%
-%%
-call(_InstanceId, _Event) ->
-    ok.
+start_link(Name = {via, Registry, InstanceId}, Module, Args, Event, Store, Options) ->
+    gen_fsm:start_link(Name, ?MODULE, {InstanceId, Module, Args, Event, Registry, Store, Options}).
 
 
 %%
 %%
 %%
-cast(_InstanceId, _Event) ->
-    ok.
+call(Name, Event) ->
+    gen_fsm:sync_send_event(Name, {'eproc_fsm$call', Event}).
 
 
 %%
 %%
 %%
-kill(_InstanceId, _Reason) ->
-    ok.
+call(Name, Event, Timeout) ->
+    gen_fsm:sync_send_event(Name, {'eproc_fsm$call', Event}, Timeout).
 
 
 %%
 %%
 %%
-suspend(_InstanceId, _Reason) ->
-    ok.
+cast(Name, Event) ->
+    gen_fsm:send_event(Name, {'eproc_fsm$cast', Event}).
 
 
 %%
 %%
 %%
-resume(_InstanceId, _Reason) ->
-    ok.
+kill(Name, Reason) ->
+    gen_fsm:send_event(Name, {'eproc_fsm$kill', Reason}).
 
 
 %%
 %%
 %%
-set_state(_InstanceId, _NewStateName, _NewStateData, _Reason) ->
-    ok.
+suspend(Name, Reason) ->
+    gen_fsm:sync_send_event(Name, {'eproc_fsm$suspend', Reason}).
+
+
+%%
+%%
+%%
+resume(Name, Reason) ->
+    gen_fsm:sync_send_event(Name, {'eproc_fsm$resume', Reason}).
+
+
+%%
+%%
+%%
+set_state(Name, NewStateName, NewStateData, Reason) ->
+    gen_fsm:sync_send_event(Name, {'eproc_fsm$set_state', NewStateName, NewStateData, Reason}).
 
 
 %%
 %%  To be used by the process implementation to sent response to a synchronous
 %%  request before the `handle_state/4` function completes.
 %%
--spec reply(state_event(), inst_ref()) -> ok.
+-spec reply(
+        state_event(),
+        inst_ref()
+        ) -> ok.
 
 reply(_Response, _InstRef) ->
     ok.
 
 
 %% =============================================================================
-%%  Callbacks for `gen_server`.
+%%  Callbacks for `gen_fsm`.
 %% =============================================================================
+
+%%
+%%  The initialization is implemented asynchronously to avoid timeouts when
+%%  restarting the engine with a lot of running fsm's.
+%%
+init({InstanceId, Module, Args, Event, Registry, Store, Options}) ->
+    State = #state{
+        inst_id     = InstanceId,
+        module      = Module,
+        registry    = Registry,
+        store       = Store,
+        options     = Options
+    },
+    self() ! {'eproc_fsm$init', Args, Event},
+    {ok, initializing, State}.
+
+
+%%
+%%  Handles the `initializing` state.
+%%
+initializing(_Event, State) ->
+    {noreply, State}.
+
+initializing(_Event, _From, State) ->
+    {reply, ok, State}.
+
+
+%%
+%%  Handles the `running` state.
+%%
+running(_Event, State) ->
+    {noreply, State}.
+
+running(_Event, _From, State) ->
+    {reply, ok, State}.
+
+
+%%
+%%  Handles the `suspended` state.
+%%
+suspended(_Event, State) ->
+    {noreply, State}.
+
+suspended(_Event, _From, State) ->
+    {reply, ok, State}.
+
+
+%%
+%%  Handles the `faulty` state.
+%%
+faulty(_Event, State) ->
+    {noreply, State}.
+
+faulty(_Event, _From, State) ->
+    {reply, ok, State}.
+
+
+%%
+%%  Not used.
+%%
+handle_event(_Event, StateName, StateData) ->
+    {noreply, StateName, StateData}.
+
+
+%%
+%%  Not used.
+%%
+handle_sync_event(_Event, _From, StateName, StateData) ->
+    {reply, not_implemented, StateName, StateData}.
+
+
+%%
+%%  Does asynchronous initialization.
+%%
+handle_info({'eproc_fsm$init', _Args, _Event}, initializing, StateData) ->
+    {noreply, running, StateData};
+
+%%
+%%  Handles FSM timers.
+%%
+handle_info({'eproc_fsm$timer', _TimerRef, _Event}, StateName, StateData) ->
+    {noreply, StateName, StateData}.
+
+
+%%
+%%  Invoked, when the FSM terminates.
+%%
+terminate(_Reason, _StateName, _StateData) ->
+    ok.
+
+
+%%
+%%  Invoked in the case of code upgrade.
+%%
+code_change(_OldVsn, StateName, StateData, _Extra) ->
+    {ok, StateName, StateData}.
 
 
 
 %% =============================================================================
 %%  Internal functions.
 %% =============================================================================
-
 
