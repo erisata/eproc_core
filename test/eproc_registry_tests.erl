27fdb272d4370db7885f90a63b297ce747fc08b3
Karolis Petrauskas <k.petrauskas@gmail.com>
2014-02-22 22:43:08 +0200

    Tests for eproc_registry.

--- test/eproc_registry_tests.erl
+++ test/eproc_registry_tests.erl
@@ -21,9 +21,85 @@
 
 
 %%
-%%  TODO
+%%  Check if eproc_registry:ref/0 works with existing configuration.
 %%
-todo_test() ->
-    ?assert(todo).
+reg_from_env_test() ->
+    ok = meck:new(eproc_core_app, []),
+    ok = meck:expect(eproc_core_app, registry_cfg, fun
+        () -> {ok, {eproc_reg_gproc, reg_args}}
+    end),
+    ?assertEqual({ok, {eproc_reg_gproc, reg_args}}, eproc_registry:ref()),
+    ?assert(meck:validate(eproc_core_app)),
+    ok = meck:unload(eproc_core_app).
+
+
+%%
+%%  Check if eproc_registry:ref/0 works with missing configuration.
+%%
+reg_undefined_test() ->
+    ok = meck:new(eproc_core_app, []),
+    ok = meck:expect(eproc_core_app, registry_cfg, fun
+        () -> undefined
+    end),
+    ?assertEqual(undefined, eproc_registry:ref()),
+    ?assert(meck:validate(eproc_core_app)),
+    ok = meck:unload(eproc_core_app).
+
+
+%%
+%%  Check if eproc_registry:ref/2 works.
+%%
+reg_explicit_test() ->
+    ?assertEqual({ok, {mod, args}}, eproc_registry:ref(mod, args)).
+
+
+%%
+%%  Check if eproc_registry:supervisor_child_specs/1 works.
+%%
+supervisor_child_specs_test() ->
+    {ok, Registry} = eproc_registry:ref(eproc_reg_gproc, []),
+    ok = meck:new(eproc_reg_gproc, []),
+    ok = meck:expect(eproc_reg_gproc, supervisor_child_specs, fun
+        ([]) -> {ok, [some, specs]}
+    end),
+    ?assertEqual({ok, [some, specs]}, eproc_registry:supervisor_child_specs(Registry)),
+    ?assert(meck:validate(eproc_reg_gproc)),
+    ok = meck:unload(eproc_reg_gproc).
+
+
+%%
+%%  Check if eproc_registry:make_new_fsm_ref/3 works.
+%%
+make_new_fsm_ref_test() ->
+    {ok, Registry} = eproc_registry:ref(eproc_reg_gproc, []),
+    ?assertEqual(
+        {ok, {via, eproc_reg_gproc, {new, [], {inst, 1}, {default, []}}}},
+        eproc_registry:make_new_fsm_ref(Registry, {inst, 1}, {default, []})
+    ).
+
+
+%%
+%%  Check if eproc_registry:make_fsm_ref/3 works.
+%%
+make_fsm_ref_test() ->
+    {ok, Registry} = eproc_registry:ref(eproc_reg_gproc, []),
+    ?assertEqual(
+        {ok, {via, eproc_reg_gproc, {fsm, [], {inst, 1}}}},
+        eproc_registry:make_fsm_ref(Registry, {inst, 1})
+    ).
+
+
+%%
+%%  Check if eproc_registry:register_fsm/3 works.
+%%
+register_fsm_test() ->
+    {ok, Registry} = eproc_registry:ref(eproc_reg_gproc, []),
+    ok = meck:new(eproc_reg_gproc, []),
+    ok = meck:expect(eproc_reg_gproc, register_fsm, fun
+        ([], {inst, 123}, [{inst, 123}, {name, n1}]) -> ok
+    end),
+    ?assertEqual(ok, eproc_registry:register_fsm(Registry, {inst, 123}, [{inst, 123}, {name, n1}])),
+    ?assert(meck:validate(eproc_reg_gproc)),
+    ok = meck:unload(eproc_reg_gproc).
 
 
