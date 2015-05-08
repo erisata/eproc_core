d7b54aaee7c4303d281deccf449c1b099e85f80c
Karolis Petrauskas <k.petrauskas@gmail.com>
2015-05-08 10:55:24 +0300

    ITest process made more interesting.

--- itest/eproc_fsm_SUITE.erl
+++ itest/eproc_fsm_SUITE.erl
@@ -228,8 +228,8 @@ test_timers(_Config) ->
 %%  Check if router works with FSM.
 %%
 test_router_integration(_Config) ->
-    OrderId = {order, erlang:node(), erlang:now()},
-    ok               = eproc_fsm__order:create(OrderId),
+    OrderId = erlang:phash2({order, erlang:node(), erlang:now()}),
+    ok               = eproc_fsm__order:create(OrderId, cust_123, [some, order, lines]),
     {ok, DeliveryId} = eproc_fsm__order:process(OrderId),
     {ok, completed}  = eproc_fsm__order:delivered(DeliveryId),
     ok.
