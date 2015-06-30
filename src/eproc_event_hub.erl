6585bbd0470a58ff5aa00ea8fa57d5f7188964c0
Julius Andrikonis <julius@andrikonis.lt>
2015-06-30 15:19:02 +0300

    Spelling.

--- src/eproc_event_hub.erl
+++ src/eproc_event_hub.erl
@@ -31,7 +31,7 @@
 %%% using Erlang distribution (uses `gen_server:abcast/2`).
 %%%
 -module(eproc_event_hub).
--bahaviour(gen_server).
+-behaviour(gen_server).
 -compile([{parse_transform, lager_transform}]).
 -export([start_link/1, start/1, listen/2, recv/3, notify/2]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
